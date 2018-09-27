5da2a89d2afd6646d1260cda5ed76ca8372fadb4
Sasan Soroush <sasan.soroush@icloud.com>
2018-09-27 21:56:08 +0330

    Initial Commit

---
+++ Numbomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Numbomb
+//
+//  Created by Sasan Soroush on 9/27/18.
+//  Copyright © 2018 Sasan Soroush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
