8d39f2fb44b6a1175040af17c55279145c1c5869
Sasan Soroush <sasansoroush@Sasans-MacBook-Pro.local>
2018-09-27 23:26:21 +0330

    initial commit

--- Numbomb/ViewController.swift
+++ Numbomb/ViewController.swift
@@ -8,18 +8,142 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
+    weak var timer: Timer?
+    var words : [UILabel] = []
+    var lastWord : UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        startTimer()
+        setupView()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func startTimer() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            
+            DispatchQueue.main.async {
+                self.generateWord()
+            }
+            
+        }
     }
+    
+    private func stopTimer() {
+        timer?.invalidate()
+    }
+    
+    
+    deinit {
+        stopTimer()
+    }
+    
+    private func generateRandomFrame(_ label : UILabel) -> CGRect {
+        
+        var stringWidth : CGFloat = 0
+        
+        if let width_ = label.text?.stringWidth {
+            stringWidth = width_
+        } else {
+            stringWidth = view.frame.width/4
+        }
+        
+        let randomX = arc4random_uniform(UInt32(view.frame.width - stringWidth))
+        
+        return CGRect(x: CGFloat(randomX), y: 0 , width: view.frame.width/4, height: 40)
+        
+    }
+    
+    private func generateWord() {
+        
+        let word : UILabel = {
+            let label = UILabel()
+            label.text = "TEST"
+            label.textColor = .red
+            label.textAlignment = .center
+            return label
+        }()
+        
+        view.addSubview(word)
+        word.frame = generateRandomFrame(word)
+        
+        words.append(word)
+        
+        animate(word: word)
+        
+    }
+    
+    private func animate(word : UILabel) {
+        //should make duration decrease at time goes on
+        UIView.animate(withDuration: 15) {
+            word.frame = CGRect(x: word.frame.minX, y: self.view.frame.height, width: word.frame.width, height: word.frame.height)
+        }
+    }
+    
+    private func chooseLastWord() {
+        
+        let sortedWords = words.sorted{$0.frame.minY < $1.frame.minY}
+        
+        guard let word = sortedWords.first else {return}
+        
+        lastWord = word
+        
+    }
+    
+    @objc private func removeLetter() {
+        
+        chooseLastWord()
+        
+        for word in words {
+            if word == lastWord {
+                word.textColor = .blue
+            }
+            word.textColor = .red
+        }
+        
+        guard var lastWordText = lastWord!.text else {return}
+        
+        if  lastWordText.count == 0 {
+            words.remove(at: 0)
+            lastWord!.removeFromSuperview()
+        } else {
+            lastWordText.remove(at: lastWordText.startIndex)
+        }
+        
+        lastWord!.text = lastWordText
 
+    }
+    
+    let button : UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .red
+        btn.addTarget(self, action: #selector(removeLetter), for: UIControlEvents.touchUpInside)
+        return btn
+    }()
+    
+    private func setupView() {
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(button)
+        
+        button.frame = CGRect(x: 0, y: view.frame.height-55, width: view.frame.width, height: 55)
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
 
